faec4c4469dcadfb5197c15550962c9680c0860b
gustavoneves12 <gustavo.neves12@gmail.com>
2016-02-22 15:23:24 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace path2d_view
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace path2d_view
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
